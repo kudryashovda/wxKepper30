399d298bbf666ee463c94aaac26c585b78c82139
am <am@gmail.rrr>
2021-11-23 16:33:31 +0300

    resize main window, disable console back

--- main.cpp
+++ main.cpp
@@ -6,14 +6,14 @@ public:
     bool OnInit() override;
 };
 
-bool MyApp::OnInit() {
+bool MyApp::OnInit() {    
     wxSetlocale(LC_ALL, ""); // to work with non-ascii chars in filenames
 
     wxLogic logic;
 
     const wxString title("Keeper");
     auto* frame = new MainFrame(title, logic);
-    const auto window_size = wxSize(800, 800);
+    const auto window_size = wxSize(1000, 800);
     frame->SetMinSize(window_size);
     frame->SetSize(window_size);
 
